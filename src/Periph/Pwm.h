f7c0f78531ccc01adab891defe1af9a9912be743
Zahorack <oliverholly11@gmail.com>
2018-06-19 21:23:59 +0200

    add pwm.h
    Signed-off-by: Zahorack <oliverholly11@gmail.com>

---
+++ src/Periph/Pwm.h
@@ -0,0 +1,62 @@
+/*
+ * Pwm.h
+ *
+ *  Created on: Jun 18, 2018
+ *      Author: Zahorack
+ */
+
+
+#ifndef PWM_H_
+#define PWM_H_
+
+#include "stm32f4xx.h"
+
+#define	PWM_STEPS		  100
+#define PULSE_1MS   	  180000  // 1ms pulse width
+#define ESC_PULSE_PERIOD  5000  // pulse period (400Hz)
+
+namespace Periph{
+
+namespace Pwms{
+enum Enum : uint8_t{
+	Pwm1 = 0,
+	Pwm2,
+	Pwm3,
+	Pwm4,
+	Pwm5,
+	Pwm6,
+	Pwm7,
+	Pwm8,
+
+
+	Size
+};
+} /* namespace Pwm */
+
+
+class Pwm {
+	const Pwms::Enum id;
+
+	void initRCC()
+	void initGpio();
+	void initTimOC();
+	void initTimTB();
+	void initPwm(uint32_t frequency);
+	void deinitPwm();
+
+
+public:
+	Pwm(Pwms::Enum id, uint32_t frequency);
+	~Pwm();
+
+	void write();
+
+
+};
+
+
+} /* namespace Periph */
+
+
+
+#endif /* PWM_H_ */
