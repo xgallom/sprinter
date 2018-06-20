40f9164122eda0432c9415ccd5915ddc242b2148
Zahorack <oliverholly11@gmail.com>
2018-06-20 03:09:18 +0200

    add proposal of Engine class
    Signed-off-by: Zahorack <oliverholly11@gmail.com>

---
+++ src/Periph/Engine.cpp
@@ -0,0 +1,65 @@
+/*
+ * Engine.cpp
+ *
+ *  Created on: Jun 18, 2018
+ *      Author: Zahorack
+ */
+
+
+#include "Engine.h"
+
+
+
+namespace Periph {
+
+
+Engine::Engine(Engines::Enum id):
+	pwm(1000),
+	id(id)
+	//Pwms::Enum(id)
+{}
+
+Engine::~Engine(){}
+
+void Engine::setSpeed(uint8_t value)
+{
+	speed = value;
+}
+
+uint8_t Engine::getSpeed()
+{
+	return speed;
+}
+
+void setDirection(Dir::Enum direction)
+{
+
+
+}
+
+void Engine::run()
+{
+	pwm.write(Pwms::Enum(id), speed);
+}
+
+void Engine::run(uint8_t value)
+{
+	speed = value;
+	pwm.write(Pwms::Enum(id), speed);
+}
+
+void Engine::run(uint8_t value, Dir::Enum direction)
+{
+	setDirection(direction);
+	speed = value;
+	pwm.write(Pwms::Enum(id), speed);
+}
+
+
+
+
+
+
+
+
+} /* namespace Periph */
---
+++ src/Periph/Engine.h
@@ -0,0 +1,70 @@
+/*
+ * Engine.h
+ *
+ *  Created on: Jun 18, 2018
+ *      Author: Zahorack
+ */
+
+#ifndef PERIPH_ENGINE_H_
+#define PERIPH_ENGINE_H_
+
+#include "stm32f4xx.h"
+#include "Pwm.h"
+
+namespace Periph {
+
+namespace Engines {
+enum Enum : uint8_t{
+		M1=0,
+		M2,
+		M3,
+		M4,
+		M5,
+		M6,
+
+		Size
+
+};
+}
+
+namespace Dir {
+	enum Enum : bool{
+		forward=0,
+		backward
+};
+}
+
+
+class Engine {
+	Pwm pwm;
+
+	const Engines::Enum id;
+	volatile uint8_t speed;
+
+
+public:
+	Engine(Engines::Enum id);
+	~Engine();
+
+	void setSpeed(uint8_t value);
+	uint8_t getSpeed();
+
+	void setDirection(Dir::Enum direction);
+
+	void run();
+	void run(uint8_t value);
+	void run(uint8_t value, Dir::Enum direction);
+
+	void stop();
+
+};
+
+
+} /* namespace Periph */
+
+
+
+
+
+
+#endif /* PERIPH_ENGINE_H_ */
