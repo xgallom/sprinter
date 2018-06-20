fc9801a0c53a92e226ddb05e1556e8e86c57d70f
Milan Gallo <milan.gallo@streamunlimited.com>
2018-06-20 14:56:07 +0200

    Add Util::State class
    Signed-off-by: Milan Gallo <milan.gallo@streamunlimited.com>

---
+++ src/Util/State.h
@@ -0,0 +1,39 @@
+/*
+ * State.h
+ *
+ *  Created on: Jun 20, 2018
+ *      Author: xgallom
+ */
+
+#ifndef UTIL_STATE_H_
+#define UTIL_STATE_H_
+
+namespace Util {
+
+struct StateMask {
+	StateMask(uint8_t offset, uint8_t mask) : offset(offset), mask(mask) {}
+
+	uint8_t offset;
+	uint8_t mask;
+};
+
+template<typename T>
+struct State {
+	T m_stateVariable;
+
+	State(T initialState = 0) : m_stateVariable(initialState) {}
+
+	bool flag(uint8_t offset) const { return m_stateVariable & (1 << offset); }
+	void setFlag(uint8_t offset) { m_stateVariable |= 1 << offset; }
+	void resetFlag(uint8_t offset) { m_stateVariable &= ~(1 << offset); }
+	void toggleFlag(uint8_t offset) { m_stateVariable ^= 1 << offset; }
+
+	T mask(StateMask stateMask) const { return (m_stateVariable >> stateMask.offset) & stateMask.mask; }
+	void setMask(StateMask stateMask, T value) {
+		m_stateVariable = (m_stateVariable & ~(stateMask.mask << stateMask.offset)) | (value << stateMask.offset);
+	}
+};
+
+} /* namespace Util */
+
+#endif /* UTIL_STATE_H_ */
